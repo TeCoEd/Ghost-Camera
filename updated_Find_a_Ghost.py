5aaad20569ba432f5ac41766496c096f7b9636ad
Dan Aldred <dan26jam@yahoo.co.uk>
2017-08-16 08:42:25 +0100

    Updates
    New Pi Camera coded added to the program + mp3 file name change

--- updated_Find_a_Ghost.py
+++ updated_Find_a_Ghost.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-#ghost evidence, 1)movement, 2)change in temp, 3)colour / light 
+
 import sys
 import os
 import time
-import picamera
+from picamera import PiCamera
 import RPi.GPIO as GPIO
 from envirophat import light, weather, motion, leds, analog
+
+camera = PiCamera()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(10, GPIO.OUT)
@@ -17,26 +19,21 @@ last_z = 0
 
 global file_name
 file_name = "0"
-analog_values = analog.read_all()
-print (analog_values)
 
 #set up a function for the camera#
 def catch_a_ghost():
-    global file_name
-    with picamera.PiCamera() as camera:
-            GPIO.output(10, GPIO.HIGH)
-            time.sleep(0.2)
-            #camera.start_preview() #remove
-            #time.sleep(2) #remove
-            print (file_name)
-            camera.capture(file_name + ".jpg")
-            time.sleep(0.3)
-            GPIO.output(10, GPIO.LOW)
-            file_name = int(file_name) + int(1)
-            file_name = str(file_name)
-            os.system('mpg321 /home/pi/wegotone.mp3 @')
+        global file_name
+    
+        GPIO.output(10, GPIO.HIGH)
+        time.sleep(0.2)
+        print (file_name)
+        camera.capture(file_name + ".jpg")
+        time.sleep(0.3)
+        GPIO.output(10, GPIO.LOW)
+        file_name = int(file_name) + int(1)
+        file_name = str(file_name)
+        os.system('mpg321 /home/pi/wegotone.mp3 @')
             
-
 ###establish the temperature of the room###
 start_temp = weather.temperature()
 print ("The room is ", start_temp)
@@ -44,7 +41,7 @@ print ("The room is ", start_temp)
 try:
     while True:
         current_temp = weather.temperature()
-        print ("Current Temerature is", current_temp)
+        print ("Current Temperature is", current_temp)
         #time.sleep(8)
 
         #Sense any movement#
